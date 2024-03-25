fdf74e1ccc31e4ef1795c9c2e4334b4916cf4b3d
PX <1209304680@qq.com>
2024-03-25 15:33:47 +0800

    初始化

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,10 @@
 import sys
 import pygame
 
+
 class AlienInvasion:
     def __init__(self):
-        '''初始化游戏并创建游戏资源'''
+        """初始化游戏并创建游戏资源"""
         pygame.init()
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))
@@ -11,7 +12,7 @@ class AlienInvasion:
         self.bg_color = (55, 125, 34)
 
     def run_game(self):
-        '''开始游戏的主循环'''
+        """开始游戏的主循环"""
         while True:
             # 侦听键鼠事件
             for event in pygame.event.get():
@@ -24,6 +25,7 @@ class AlienInvasion:
             # 让最近绘制的屏幕可见
             pygame.display.flip()
             self.clock.tick(60)
+
 
 if __name__ == '__main__':
     AlienInvasion().run_game()
