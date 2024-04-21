408ed3213dd9caa48904382aafa8099209d20d33
MuttonString <1623492183@qq.com>
2024-04-21 15:39:58 +0800

    最终整合

--- ship.py
+++ ship.py
@@ -1,48 +1,57 @@
 import pygame
-from settings import Settings
+from pygame.sprite import Sprite
 
-class Ship:
-  """管理飞船的类"""
-  def __init__(self,ai_game):
-    """初始化飞船并设置其初始位置"""
-    self.screen = ai_game.screen
-    self.screen_rect = ai_game.screen.get_rect()
-    self.settings = ai_game.settings
-    
-    # 加载飞船图像并获取其外接矩形
-    self.image = pygame.image.load('./imgs/plane.png')
-    self.rect = self.image.get_rect()
-    
-    # 每艘新飞船都放在屏幕底部的中央
-    self.rect.midbottom = self.screen_rect.midbottom
-    
-    #移动标识
-    self.moving_right = False
-    self.moving_left = False
-    self.moving_top = False
-    self.moving_bottom = False
-    
-    self.x = float(self.rect.x)
-    self.y = float(self.rect.y)
-  
-  def blitme(self):
-    """在指定位置绘制飞船"""
-    self.screen.blit(self.image,self.rect)
-    
-  def update(self):
-    """根据移动标志调整飞船位置"""
-    if self.moving_right and self.rect.right < self.screen_rect.right:
-      self.x += self.settings.ship_speed
-      self.rect.x = self.x
-    elif self.moving_left and self.rect.left > 0:
-      self.x -= self.settings.ship_speed
-      self.rect.x = self.x
-    elif self.moving_top and self.rect.top > 0:
-      self.y -= self.settings.ship_speed
-      self.rect.y = self.y
-    elif self.moving_bottom and self.rect.bottom < self.screen_rect.bottom:
-      self.y += self.settings.ship_speed
-      self.rect.y = self.y
-  
-    
-  
+
+class Ship(Sprite):
+    def __init__(self, ai_game, half_size=False):
+        '''初始化飞船并设置其初始位置'''
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # 加载飞船图像
+        self.image = pygame.image.load('images/ship.png')
+        # 缩放图像
+        if half_size:
+            self.image = pygame.transform.scale(
+                self.image, (self.settings.ship_width / 2, self.settings.ship_height / 2))
+        else:
+            self.image = pygame.transform.scale(
+                self.image, (self.settings.ship_width, self.settings.ship_height))
+        self.rect = self.image.get_rect()
+
+        # 每艘新飞船都放在屏幕底部中央
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # 在飞船的属性x,y中存储浮点数
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        '''根据移动标志调整飞船的位置'''
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def blitme(self):
+        '''在指定位置绘制飞船'''
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
